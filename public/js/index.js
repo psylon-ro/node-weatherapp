7db3c106964b7f529374da11eb341399d2009ea4
psylon-ro <72932849+psylon-ro@users.noreply.github.com>
2022-07-24 16:41:43 +0530

    setup for herkou

--- public/js/index.js
+++ public/js/index.js
@@ -13,7 +13,7 @@ messone.textContent='Loading...'
 messtwo.textContent=''
 
 
-fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+fetch(`/weather?address=${location}`).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             messone.textContent= data.error
